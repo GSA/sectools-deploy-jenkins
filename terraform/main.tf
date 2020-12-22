ea50b363b496135ba978bba3b1a7c4e960c37670
ericrevans <eric.evans@gsa.gov>
2020-12-22 15:46:46 -0500

    ensure bucket is encrypted and versioned

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,29 @@ provider "aws" {
   region = var.region
 }
 
+//kms key for encrypted buckets
+resource "aws_kms_key" "jenkins_backup_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+
 //s3 bucket for backups
 resource "aws_s3_bucket" "jenkins_backup" {
   bucket = "${var.project}-${var.app_env}-jenkins-backup"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.jenkins_backup_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
 module "jenkins" {
